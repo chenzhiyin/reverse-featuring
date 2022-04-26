f66e401b08bdc97b2e1593923658638302b83d4a
chenzhiyin <zhiyin.chen@intel.com>
2022-04-26 13:46:30 +0800

    Init Reverse Featuring

---
+++ generate_data.py
@@ -0,0 +1,145 @@
+import argparse
+import os
+import time
+import uuid
+
+import pandas as pd
+
+# Definition of some constants
+USER_COLUMNS = ['user_id', 'gender', 'visit_city', 'avg_price',
+                'is_supervip', 'ctr_30', 'ord_30', 'total_amt_30']
+ITEM_COLUMNS = ['shop_id', 'item_id', 'city_id', 'district_id', 'shop_atoi_id', 'shop_geohash6',
+                'shop_geohash12', 'brand_id', 'c_1_id', 'merge_standard_food_id', 'rnk_7', 'rnk_30', 'rnk_90']
+BEHAVIOR_CLOUMNS = ['shop_id_list', 'item_id_list', 'c_1_id_list', 'merge_standard_food_id_list', 'brand_id_list',
+                    'price_list', 'shop_aoi_id_list', 'shop_geohash6_list', 'timediff_list', 'hours_list',
+                    'time_type_list', 'weekdays_list']
+REQUEST_CLOUMNS = ['times', 'hours', 'time_type', 'weekdays', 'geohash12']
+LABEL_COLUMN = ['clicked']
+COLUMNS = LABEL_COLUMN + USER_COLUMNS + \
+    ITEM_COLUMNS + BEHAVIOR_CLOUMNS + REQUEST_CLOUMNS
+
+
+def get_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_location',
+                        help='Full path of train data',
+                        required=False,
+                        default='./data')
+
+    return parser
+
+
+def output_log_files(info):
+    log_files_map = {
+        'user_log_info_list': "./data/logs/user_service_log",
+        'item_log_info_list': "./data/logs/item_service_log",
+        'behavior_log_info_list': "./data/logs/behavior_service_log",
+        'request_log_info_list': "./data/logs/request_service_log"
+    }
+
+    for key in log_files_map.keys():
+        f = open(log_files_map[key], 'a', encoding='utf-8')
+        f.writelines(info[key])
+        f.close()
+
+        info[key] = list()
+
+    return
+
+
+def append_log_info(data, key, log_info):
+    if key not in data.keys():
+        data[key] = list()
+
+    data[key].append(log_info + '\n')
+
+
+def convert_data_logs(data, log_info, names):
+    for column in names:
+        value = "NULL" if pd.isna(data[column]) else str(data[column])
+        log_info += ' ' + value
+
+    return log_info
+
+
+def convert_user_logs(data, info):
+    log_info = f"{info['times']} INFO {info['batch_id']} user_info_service"
+    log_info = convert_data_logs(data, log_info, USER_COLUMNS)
+
+    append_log_info(info, 'user_log_info_list', log_info)
+
+    return
+
+
+def convert_item_logs(data, info):
+    log_info = f"{info['times']} INFO {info['request_id']} {info['batch_id']} item_info_service"
+    log_info = convert_data_logs(data, log_info, ITEM_COLUMNS)
+
+    append_log_info(info, 'item_log_info_list', log_info)
+
+    return
+
+
+def covert_behavior_logs(data, info):
+    log_info = f"{info['times']} INFO {info['batch_id']} behavior_info_service"
+    log_info = convert_data_logs(data, log_info, BEHAVIOR_CLOUMNS)
+
+    append_log_info(info, 'behavior_log_info_list', log_info)
+
+    return
+
+
+def covert_request_logs(data, info):
+    log_info = f"{info['times']} INFO {info['request_id']} {info['batch_id']} recommend_service {data['clicked']}"
+    log_info = convert_data_logs(data, log_info, REQUEST_CLOUMNS)
+
+    append_log_info(info, 'request_log_info_list', log_info)
+
+    return
+
+
+def convert_data(index, data, info):
+    # for values in chunk.
+    local_time = time.localtime(int(data['times']))
+    info['times'] = time.strftime("%Y-%m-%d %H:%M:%S", local_time)
+    batch_key = str(data['times']) + str(data['geohash12']) + str(data['user_id'])
+    request_key = batch_key + str(data['item_id']) + str(index)
+    info['request_id'] = str(uuid.uuid5(uuid.NAMESPACE_OID, request_key).hex)
+
+    if 'user_id' not in info.keys() or info['user_id'] != data['user_id']:
+        info['user_id'] = str(data['user_id'])
+        info['batch_id'] = str(uuid.uuid5(uuid.NAMESPACE_OID, batch_key).hex)
+        covert_behavior_logs(data, info)
+        convert_user_logs(data, info)
+
+    convert_item_logs(data, info)
+    covert_request_logs(data, info)
+
+    return
+
+
+def generate_log_file(file):
+    info = dict()
+
+    reader = pd.read_csv(file, index_col=False, names=COLUMNS,
+                         iterator=True, chunksize=10, header=None)
+    for chunk in reader:
+        for index, row in chunk.iterrows():
+            convert_data(index, row, info)
+
+        output_log_files(info)
+
+    return
+
+
+def main():
+    files = os.listdir(args.data_location)
+    for file in files:
+        if not os.path.isdir(file) and file.endswith(".csv"):
+            generate_log_file(args.data_location + '/' + file)
+
+
+if __name__ == '__main__':
+    parser = get_arg_parser()
+    args = parser.parse_args()
+    main()
---
+++ train.py
@@ -0,0 +1,107 @@
+import argparse
+import os
+from random import random
+import time
+import uuid
+from sdv.tabular import ctgan
+
+import pandas as pd
+
+# Definition of some constants
+USER_COLUMNS = ['user_id', 'gender', 'visit_city', 'avg_price',
+                'is_supervip', 'ctr_30', 'ord_30', 'total_amt_30']
+ITEM_COLUMNS = ['shop_id', 'item_id', 'city_id', 'district_id', 'shop_atoi_id', 'shop_geohash6',
+                'shop_geohash12', 'brand_id', 'c_1_id', 'merge_standard_food_id', 'rnk_7', 'rnk_30', 'rnk_90']
+BEHAVIOR_CLOUMNS = ['shop_id_list', 'item_id_list', 'c_1_id_list', 'merge_standard_food_id_list', 'brand_id_list',
+                    'price_list', 'shop_aoi_id_list', 'shop_geohash6_list', 'timediff_list', 'hours_list',
+                    'time_type_list', 'weekdays_list']
+REQUEST_CLOUMNS = ['times', 'hours', 'time_type', 'weekdays', 'geohash12']
+LABEL_COLUMN = ['clicked']
+COLUMNS = LABEL_COLUMN + USER_COLUMNS + \
+    ITEM_COLUMNS + BEHAVIOR_CLOUMNS + REQUEST_CLOUMNS
+
+
+def get_arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_location',
+                        help='Full path of train data',
+                        required=False,
+                        default='./data')
+
+    parser.add_argument('--fraction',
+                        help='sample data ratio',
+                        type=float,
+                        default=1)
+
+    parser.add_argument('--chunk_size',
+                        help='sample data ratio',
+                        type=int,
+                        default=10)
+
+    parser.add_argument('--sample',
+                        help='sample dataset to train model',
+                        action='store_true')
+
+    return parser
+
+
+def sample_data(index, data, info):
+    if 'user_id' not in info.keys() or info['user_id'] != data['user_id']:
+        info['is_selected'] = args.fraction > random()
+        info['user_id'] = data['user_id']
+
+    return info['is_selected']
+
+
+def generate_sample_file(file):
+    sample_file = './data/sample/' + \
+        str(file).split('/')[-1].split(".")[-2]+"_sample.csv"
+    info = dict()
+
+    reader = pd.read_csv(file, index_col=False, names=COLUMNS,
+                         iterator=True, chunksize=args.chunk_size, header=None)
+    for chunk in reader:
+        sample_list = list()
+        for index, row in chunk.iterrows():
+            if sample_data(index, row, info):
+                sample_list.append(index % args.chunk_size)
+
+        if len(sample_list) > 0:
+            chunk.iloc[sample_list].to_csv(
+                sample_file, header=0, index=0, mode='a')
+
+    return
+
+
+def sample():
+    files = os.listdir(args.data_location)
+    for file in files:
+        if not os.path.isdir(file) and file.endswith(".zip"):
+                generate_sample_file(args.data_location + '/' + file)
+
+
+def train():
+    data_path = args.data_location+'/'+'sample'
+    files = os.listdir(data_path)
+    for file_name in files:
+        file = data_path + '/' + file_name
+        data = pd.read_csv(file)
+        model = ctgan.CTGAN()
+        model.fit(data)
+        model.save(file_name.split('.')[-2]+'.pkl')
+
+    return
+
+
+def main():
+    if args.sample:
+        sample()
+
+    sample()
+    # train()
+
+
+if __name__ == '__main__':
+    parser = get_arg_parser()
+    args = parser.parse_args()
+    main()
